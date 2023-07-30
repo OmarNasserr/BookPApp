3b2cdfa45c6ba81075cdc987ea75a13389222970
OmarNasserr <omarnasserr44@gmail.com>
2023-07-30 10:07:34 +0300

    (ADD) Side Drawer was added

--- booky/lib/Screens/HomePage.dart
+++ booky/lib/Screens/HomePage.dart
@@ -12,6 +12,7 @@ import '../Controllers/GeoLocationControllers/GeoLocationAPIs.dart';
 import '../Controllers/PlaygroundController/NearestPlaygroundController.dart';
 import '../Controllers/ViewController/ViewController.dart';
 import '../Widgets/HomePage/DistanceSlider.dart';
+import '../Widgets/HomePage/SideDrawer.dart';
 import '../Widgets/frquent_used_widgets/ImageSlider.dart';
 import '../Widgets/HomePage/SearchBar.dart';
 import '../Widgets/HomePage/SectionListView.dart';
@@ -25,15 +26,26 @@ class HomePage extends StatelessWidget {
   CitiesController citiesController = Get.find<CitiesController>();
   SliderImageController sliderImageController =
       Get.find<SliderImageController>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   double range = 0;
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
       child: Scaffold(
         resizeToAvoidBottomInset: false,
+        key: _scaffoldKey,
+        drawer: ClipRRect(
+          borderRadius: BorderRadius.vertical(
+              top: Radius.circular(32.0), bottom: Radius.circular(32.0)),
+          child: Drawer(
+            width: size.width / 1.2,
+              child: SideDrawer(scaffoldKey: _scaffoldKey,),
+          ),
+        ),
         body: SafeArea(
           child: LayoutBuilder(builder: (context, constraints) {
             double screenHeight = constraints.maxHeight;
@@ -46,18 +58,38 @@ class HomePage extends StatelessWidget {
                     height: viewController.getPortrait(
                         screenHeight / 100, screenWidth / 100),
                   ),
-                  searchBar(
-                    viewController: viewController,
-                    screenHeight: screenHeight,
-                    screenWidth: screenWidth,
-                    iconData: Icons.search,
-                    onSubmitted: (String str) {
-                      debugPrint("STRRR $str");
-                    },
-                    onTap: () {},
-                    onChanged: (String str) {
-                      debugPrint(str);
-                    },
+                  SizedBox(
+                  width: viewController.getPortrait(screenWidth, screenWidth),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MaterialButton(
+                          padding: const EdgeInsets.all(0),
+                          minWidth: 0,
+                          onPressed: () {
+                            _scaffoldKey.currentState?.openDrawer();
+                          },
+                          child: Container(
+                            child: Icon(Icons.menu,color: Colors.white,size:viewController.getPortrait(
+                                screenWidth / 10, screenHeight / 10) ,),
+                          ),
+                        ),
+                        SizedBox(width: viewController.getPortrait(screenWidth/35, screenWidth / 1.4),),
+                        searchBar(
+                          viewController: viewController,
+                          screenHeight: screenHeight,
+                          screenWidth: screenWidth,
+                          iconData: Icons.search,
+                          onSubmitted: (String str) {
+                            debugPrint("STRRR $str");
+                          },
+                          onTap: () {},
+                          onChanged: (String str) {
+                            debugPrint(str);
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                   SizedBox(
                     height: viewController.getPortrait(
@@ -137,4 +169,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
