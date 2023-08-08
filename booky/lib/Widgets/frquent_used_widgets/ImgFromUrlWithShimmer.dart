d2bd8fad3baa9fb015f59e5eed044d67189ea0bb
OmarNasserr <omarnasserr44@gmail.com>
2023-08-08 13:36:38 +0300

    (UPDT) removed unused packages

--- booky/lib/Widgets/frquent_used_widgets/ImgFromUrlWithShimmer.dart
+++ booky/lib/Widgets/frquent_used_widgets/ImgFromUrlWithShimmer.dart
@@ -1,6 +1,8 @@
 import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 
 class ImgFromNetwork extends StatelessWidget {
   final String imgUrl;
