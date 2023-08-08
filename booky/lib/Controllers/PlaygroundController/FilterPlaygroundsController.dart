24be48c4659eb9f16bb29f50ade7d46bd67b605d
OmarNasserr <omarnasserr44@gmail.com>
2023-08-08 13:34:51 +0300

    (UPDT) updated filterPlayground remote service func, added filtering by playground's owner username

--- booky/lib/Controllers/PlaygroundController/FilterPlaygroundsController.dart
+++ booky/lib/Controllers/PlaygroundController/FilterPlaygroundsController.dart
@@ -24,6 +24,12 @@ class FilterPlaygroundsController extends GetxController {
     return filterPlaygroundController;
   }
 
+  @override
+  void onInit() async {
+    await filterPlaygrounds(ownerUsername: 'admin');
+    super.onInit();
+  }
+
   /// we can filter playgrounds with multiple fields
   /// therefore this function can be used for different purposes such as filtering
   /// by city name, governorate name or search by open time and close time
@@ -31,25 +37,29 @@ class FilterPlaygroundsController extends GetxController {
   /// as the rest of the fields have empty default values
   Future<void> filterPlaygrounds(
       {pageNumber = 1,
-        pageSize = 10,
-        String cityName='',
-        String governorateName='',
-        String openTime='',///openTime and closeTime should follow hh:mm:ss format
-        String closeTime='',///ex: '10:00:00','12:03:60',etc
-        String  pricePerHour='',
-      }) async {
+      pageSize = 10,
+      String cityName = '',
+      String governorateName = '',
+      String openTime = '',
+
+      ///openTime and closeTime should follow hh:mm:ss format
+      String closeTime = '',
+
+      ///ex: '10:00:00','12:03:60',etc
+      String pricePerHour = '',
+      String ownerUsername = ''}) async {
     var a = Get.put(AlertsAndLoadingControllers());
     Get.find<AlertsAndLoadingControllers>().togglePlaygroundFilterLoading();
     var filterPlaygrounds =
-    await FilterPlaygroundsRemoteService.filterPlaygrounds(
-        pageNumber: pageNumber,
-        pageSize: pageSize,
-      cityName: cityName,
-      governorateName: governorateName,
-      openTime: openTime,
-      closeTime: closeTime,
-      pricePerHour: pricePerHour
-    );
+        await FilterPlaygroundsRemoteService.filterPlaygrounds(
+            pageNumber: pageNumber,
+            pageSize: pageSize,
+            cityName: cityName,
+            governorateName: governorateName,
+            openTime: openTime,
+            closeTime: closeTime,
+            pricePerHour: pricePerHour,
+            ownerUsername: ownerUsername);
     Get.find<AlertsAndLoadingControllers>().togglePlaygroundFilterLoading();
     if (filterPlaygrounds != null) {
       setFilterPlaygroundController(filterPlaygrounds);
